96d4c580a0c7458dbd37fb2637e3eea7a8b348fc
sajit <sajit.kunnumkal@gmail.com>
2011-07-07 15:22:56 -0400

    En.dict issue

--- src/main/java/com/webgearz/tb/controllers/StaticPageController.java
+++ src/main/java/com/webgearz/tb/controllers/StaticPageController.java
@@ -1,16 +1,60 @@
 package com.webgearz.tb.controllers;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.ServletContextAware;
 
-public class StaticPageController {
+
+@Controller
+public class StaticPageController implements ServletContextAware{
+	private static final List<String> supportedLanguages = Arrays.asList("en","fr");
+	private static final Log log = LogFactory.getLog(StaticPageController.class);
 	
-	@RequestMapping(value="/resources/aloha",method=RequestMethod.GET)
-	public File getDictFile(){
+	@RequestMapping(method=RequestMethod.GET,value="/resources/aloha/i18n/{language}.dict")
+	@ResponseBody
+	public byte[] getDictFile(@PathVariable("language")final String language,HttpServletRequest request) throws IOException{
 		
-		return null;
+		log.info("Hits the Controller");
+		if(language==null)
+			throw new RuntimeException("Cannot have empty language");
+		if(!supportedLanguages.contains(language))
+			throw new RuntimeException("Unsupported language");
+	
+		String relativeFilepath = "resources/aloha/i18n/"+language+".dict";
+		
+		
+		URL url = context.getResource(relativeFilepath);
+		InputStream is = url.openStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte buff[] = new byte[512];
+		while (is.read(buff, 0, buff.length) > 0) {
+			baos.write(buff);
+		}
+		return baos.toByteArray();
+	}
+
+	private ServletContext context;
+	@Override
+	@Autowired
+	public void setServletContext(ServletContext servletContext) {
+		this.context = servletContext;
+		
 	}
 	
 	
